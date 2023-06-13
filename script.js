1cfd983b9df12eacd16e18424943812c66d65df3
Karel Šťastný <karell29@icloud.com>
2023-06-13 12:08:26 +0200

    udělány částečné styly

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-// Nové pole pro uložen pokemonu
-let pokemons = []
+
 
 const pokeContainer = document.querySelector(".pokemon-list")
 const url = "https://pokeapi.co/api/v2/pokemon"
-const number = 2
-const form = document.querySelector("form")
-const search = document.querySelector(".search")
-
-let check = document.querySelector(".checkbox")
+let requiredNumber = document.querySelector(".number")
+let number = ""
 
 
+// Zjištění čísla z políčka pro počet pokemonů
+    requiredNumber.addEventListener("input", (e) => {
+        number = e.target.value
+
+    // Získání všech pokemonu
+    const getAllPokemon = async () =>{
+
+    // Cyklus na projetí všech pokemonu za lomítkem
+    for (let x = 1; x <= number; x++ ){
+        
+        const res = await fetch (`${url}/${x}`)
+        const pokemon = await res.json()
+
+        // Výpis do stránky
+        const poke = document.createElement("div")
+
+        poke.innerHTML = `
+        <div class="card" id="${pokemon.id} " >
+            <h2>${pokemon.name}</h2>
+            <p>${pokemon.id}</p>
+        
+            <img class="img1 active" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            <img class="img2" src="${pokemon.sprites.other.home.front_shiny}" alt="${pokemon.name}">
+        </div>
+        `
+        pokeContainer.appendChild(poke)
+    }
+
+}
+
+getAllPokemon()
+
+    })
+
+   
+    
 
 
 
-// Change Img
+
+
+
+
+// Změna obrázku za shiny form
 const changeImg = () => {
-    // let allCard = document.querySelectorAll(".card")
 
-    // console.log(allCard);
-
+    // Změny obrázků
     let imgDefault = document.querySelectorAll(".img1")
     let imgShiny = document.querySelectorAll(".img2")
 
@@ -30,57 +64,19 @@ const changeImg = () => {
         oneShiny.classList.toggle("active")
     })
 
+    // Změna hodnoty Tlačítka
 
-}
+    let button = document.querySelector(".changeButton")
 
+    button.classList.toggle("activeButton")
 
+    console.log(button.classList.value.includes("activeButton"));
 
-
-
-// Cyklem se zova spustí výše uvedená funkce na získání pokemonu do konečného čísla
-
-
-const getAllPokemon = async () =>{
-    for (let x = 1; x <= number; x++ ){
-        
-        const res = await fetch (`${url}/${x}`)
-        const pokemon = await res.json()
-
- 
-
-
-
-        // Výpis do stránky
-        const poke = document.createElement("div")
-
-        poke.innerHTML = `
-        <div class="card" id="${pokemon.id} " >
-            <h2>${pokemon.name}</h2>
-            <p>${pokemon.id}</p>
-        
-
-            <img class="img1 active" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-            <img class="img2" src="${pokemon.sprites.other.home.front_shiny}" alt="${pokemon.name}">
-
-   
-            
-            <p></p>
-
-            
-        </div>
-        `
-
-
-        pokeContainer.appendChild(poke)
-
-      
+    if(button.classList.value.includes("activeButton")){
+        button.textContent = "Shine Form"
+    }else{
+        button.textContent = "Default Form"
     }
 
+
 }
-
-
-
-getAllPokemon()
-
-
-
